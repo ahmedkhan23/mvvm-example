cd15d1b79e3ddbc11a9dabe1ef84c0a2bc0173f6
ahmedkhan23 <ahmed1.khan@gmail.com>
2024-12-03 16:14:37 -0500

    - adding permission for internet in manifest - adding coil image loading lib in gradle files

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,6 +55,7 @@ dependencies {
     implementation(libs.koin.android)
     implementation(libs.koin.androidx.compose)
     implementation(libs.bundles.networking)
+    implementation(libs.coil.compose)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)

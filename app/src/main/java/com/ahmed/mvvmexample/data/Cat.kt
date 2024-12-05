37e03c782b94ff52951449256144f6fba0afb550
ahmedkhan23 <ahmed1.khan@gmail.com>
2024-12-05 09:20:04 -0500

    - missing Serializable annotation on Cat data class

--- app/src/main/java/com/ahmed/mvvmexample/data/Cat.kt
+++ app/src/main/java/com/ahmed/mvvmexample/data/Cat.kt
@@ -1,7 +1,9 @@
 package com.ahmed.mvvmexample.data
 
 import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
 
+@Serializable
 data class Cat(
     @SerialName("createdAt")
     val createdAt: String = "",

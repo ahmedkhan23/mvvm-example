0760a9d85a30c3da522e94840f88f26757e80a0f
ahmedkhan23 <ahmed1.khan@gmail.com>
2024-12-10 22:24:51 -0500

    - updating formatting ...

--- app/src/main/java/com/ahmed/mvvmexample/data/Cat.kt
+++ app/src/main/java/com/ahmed/mvvmexample/data/Cat.kt
@@ -5,14 +5,9 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class Cat(
-    @SerialName("createdAt")
-    val createdAt: String = "",
-    @SerialName("_id")
-    val id: String,
-    @SerialName("owner")
-    val owner: String = "",
-    @SerialName("tags")
-    val tags: List<String>,
-    @SerialName("updatedAt")
-    val updatedAt: String = ""
+    @SerialName("createdAt")  val createdAt: String = "",
+    @SerialName("_id") val id: String,
+    @SerialName("owner") val owner: String = "",
+    @SerialName("tags") val tags: List<String>,
+    @SerialName("updatedAt") val updatedAt: String = ""
 )
